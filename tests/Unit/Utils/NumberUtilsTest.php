c7e2ee8a548117d1374f9b81e04940b0cdcc7497
Carsten Schindler <cschindl@gmx.de>
2022-06-29 19:20:31 +0200

    Fix mutation tests

--- tests/Unit/Utils/NumberUtilsTest.php
+++ tests/Unit/Utils/NumberUtilsTest.php
@@ -19,4 +19,52 @@ class NumberUtilsTest extends UnitTestCase
 
         self::assertEquals(100, $number);
     }
+
+    /** @test */
+    function it_can_ensure_with_minimum_range()
+    {
+        $number = NumberUtils::ensureRange(10, 100, null);
+
+        self::assertEquals(100, $number);
+    }
+
+    /** @test */
+    function it_can_ensure_with_minimum_and_exclusive_minimum_range()
+    {
+        $number = NumberUtils::ensureRange(10, 100, null, true);
+
+        self::assertEquals(101, $number);
+    }
+
+    /** @test */
+    function it_can_ensure_with_maximum_range()
+    {
+        $number = NumberUtils::ensureRange(100, null, 50);
+
+        self::assertEquals(50, $number);
+    }
+
+    /** @test */
+    function it_can_ensure_with_maximum_and_exclusive_maximum_range()
+    {
+        $number = NumberUtils::ensureRange(100, null, 50, null, true);
+
+        self::assertEquals(49, $number);
+    }
+
+    /** @test */
+    function it_can_ensure_with_minimum_and_maximum_exclusive_both_range()
+    {
+        $number = NumberUtils::ensureRange(40, 10, 50, true, true);
+
+        self::assertEquals(40, $number);
+    }
+
+    /** @test */
+    function it_can_ensure_with_multipl_of_range()
+    {
+        $number = NumberUtils::ensureRange(10, null, null, null, null, 3);
+
+        self::assertEquals(9, $number);
+    }
 }
